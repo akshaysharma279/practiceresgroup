c46b4bedd4eccf45f5d6fb534f1ca187f4348bbf
Akshay Sharma <akshayraichand0010@gmail.com>
2025-09-08 18:27:43 +0530

    added

---
+++ env/dev/main.tf
@@ -0,0 +1,5 @@
+module "rg" {
+  source      = "../../module/resourcegroup"
+  rg_name     = "akkirg1"
+  rg_location = "west Europe"
+}
---
+++ module/resourcegroup/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "akkieres" {
+    name = var.rg_name
+    location = var.rg_location
+}
